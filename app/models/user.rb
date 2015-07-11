809d34243aff65f1dbb3d365cc15d7e39aea6098
Toby Alden <toby.alden@gmail.com>
2015-07-10 17:25:25 -0700

    Changed default avatar to white square.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_many :posts
   validates_presence_of :name
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "http://orig09.deviantart.net/5d81/f/2013/125/7/2/kim_jong_un_by_jlr758-d649mib.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "http://i.imgur.com/Sidi5sT.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
